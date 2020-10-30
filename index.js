41bdf8564b9a3466b30f0ca51716b18a7634e812
tiagokriok <tiagoacarioca@hotmail.com>
2020-10-30 17:54:41 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import QRCode from 'qrcode';
+
+const generateQR = async (text) => {
+  try {
+    console.log(
+      await QRCode.toFile('images/qrcode.png', text, {
+        color: { dark: '#0345B6', light: '#0095FF' },
+        width: 600,
+        margin: 0.5,
+      })
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+generateQR('Tiago Sales de Azevedo Carioca');
